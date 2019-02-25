dc71335585399473a2155919dfd4cbac08cb1b38
Luca Gessi <lucagessi90@gmail.com>
2019-02-25 20:28:07 +0100

    Some new files

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+	var basic_url = "https://www.googleapis.com/books/v1/volumes?q=";
+	var public_key = "&key=AIzaSyCMMp3tKEmoNM81CEtQIruV4H5H7HjZMOE";
+    $( "#search-form" ).submit(function(e)
+	{
+		//alert("Ciao bel");
+		var txt = $( "#search-form input" ).val();
+		if (txt != "")
+		{
+			var compleateUrl = basic_url+txt+public_key;
+			console.log("compleateUrl");
+			$.get("ajax/test.html", function( data ) {
+				//$( ".result" ).html( data );
+				alert( "Load was performed." );
+			});
+		}else
+		{
+			console.log("Input vuoto!");
+		}
+
+		return false;
+	});
+
+	jQuery.get()
+});
