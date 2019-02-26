3468aa42555318bfd4615ed1230efa67c3912886
Luca Gessi <lucagessi90@gmail.com>
2019-02-26 07:25:51 +0100

    Deleted api key

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 $( document ).ready(function() {
 	var basic_url = "https://www.googleapis.com/books/v1/volumes?q=";
-	var public_key = "&key=AIzaSyCMMp3tKEmoNM81CEtQIruV4H5H7HjZMOE";
+	//var public_key = "&key=AIzaSyCMMp3tKEmoNM81CEtQIruV4H5H7HjZMOE";
+	var use_key = "";
     $( "#search-form" ).submit(function(e)
 	{
 		//alert("Ciao bel");
-		var txt = $( "#search-form input" ).val();
-		if (txt != "")
+		var input_text = $( "#search-form input" ).val();
+		if (input_text != "")
 		{
-			var compleateUrl = basic_url+txt+public_key;
+			var compleateUrl = basic_url+input_text+use_key;
 			console.log(compleateUrl);
 			$.get(compleateUrl, function( data ) {
 				//$( ".result" ).html( data );
 				alert( "Load was performed." );
 			});
+			/*.done(function() {
+				alert( "second success" );
+			  })
+			  .fail(function() {
+				alert( "error" );
+			  })
+			  .always(function() {
+				alert( "finished" );
+			  });*/
 		}else
 		{
 			console.log("Input vuoto!");
@@ -21,5 +31,4 @@ $( document ).ready(function() {
 		return false;
 	});
 
-	jQuery.get()
 });
