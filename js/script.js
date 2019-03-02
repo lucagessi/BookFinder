3238651ba47cada886d4467e20ad13c22a54640d
Luca Gessi <lucagessi90@gmail.com>
2019-03-02 11:21:56 +0100

    Modifica gui. Responsive books

--- js/script.js
+++ js/script.js
@@ -13,36 +13,73 @@ $( document ).ready(function() {
 			var compleateUrl = basic_url+input_text+use_key;
 			console.log(compleateUrl);
 			$.get(compleateUrl, function( data ) {
-				//$( ".result" ).html( data );
-				//alert( "Load was performed." );
 				console.log("Results"+data.totalItems);
-				//$("#results").html("<div class=\"book-result col col-centered\">"+ data.items +"</div>")
 				var book_container = document.getElementById("results");
 				book_container.innerHTML = "";
 				data.items.forEach(book => {
 					console.log(book.volumeInfo.title);
-					var node = document.createElement("div");
-					node.classList.add("col-sm-6");     
-					var imgLink = book.volumeInfo.imageLinks.smallThumbnail;
-					var imgHTML = "<img src=\""+imgLink+"\" >";
-					var nodeContent = "<div class=\"col-8\" ><div class=\"book-info\">"+book.volumeInfo.title+"</div><div>"
-					+"<a href=\""+ book.volumeInfo.canonicalVolumeLink +"\" target=\"_blank\" class=\"btn btn-info book-link\" role=\"button\">See this Book</a>"+"</div></div></div>";
-					nodeContent = "<div class=\"row book\" ><div class=\"col-4\" >"+imgHTML+"</div>"+nodeContent;
+					// Nodo Book
+					var bookNode = document.createElement("div"); 
+					bookNode.classList.add("book","col-12","col-sm-12","col-lg-6");
+					// Nodo contenitore Book
+					var bookContainerNode = document.createElement("div"); 
+					bookContainerNode.classList.add("book-container");
+					// Nodo image Book
+					var bookImageNode = document.createElement("div"); 
+					bookImageNode.classList.add("book-image");
+					bookImageNode.innerHTML = "<img src=\""+book.volumeInfo.imageLinks.smallThumbnail+"\" >";
+					// Nodo Book background
+					var bookBackgroundNode = document.createElement("div"); 
+					bookBackgroundNode.classList.add("book-background");
+					// Nodo Book info
+					var bookInfoNode = document.createElement("div"); 
+					bookInfoNode.classList.add("book-info");
+					// Nodo Book title
+					var bookTitleNode = document.createElement("div"); 
+					bookTitleNode.classList.add("book-title");
+					if (book.volumeInfo.title.length > 80)
+					{
+						bookTitleNode.innerHTML = book.volumeInfo.title.substring(0,80)+"...";
+					}else
+					{
+						bookTitleNode.innerHTML = book.volumeInfo.title;
+					}
+					// Nodo Book by
+					var bookByNode = document.createElement("div"); 
+					bookByNode.classList.add("book-by");
+					if (typeof book.volumeInfo.authors !== 'undefined')
+					{
+						bookByNode.innerHTML = "By: "+book.volumeInfo.authors[0];
+					}else
+					{
+						bookByNode.innerHTML = "By: unknown";
+					}
+					// Nodo Book pub by
+					var bookPubByNode = document.createElement("div"); 
+					bookPubByNode.classList.add("book-pub-by");
+					bookPubByNode.innerHTML = "Published By: "+book.volumeInfo.publisher;
+					// Nodo Book link
+					var bookLinkNode = document.createElement("div"); 
+					bookLinkNode.classList.add("book-link");
+					bookLinkNode.innerHTML = "<a href=\""+ book.volumeInfo.canonicalVolumeLink +"\" target=\"_blank\" class=\"btn\" role=\"button\">See this Book</a>";
+					// Creo lista nodi
+					bookInfoNode.appendChild(bookTitleNode);
+					bookInfoNode.appendChild(bookByNode);
+					bookInfoNode.appendChild(bookPubByNode);
+					bookInfoNode.appendChild(bookLinkNode);
+					bookContainerNode.appendChild(bookImageNode);
+					bookContainerNode.appendChild(bookBackgroundNode);
+					bookContainerNode.appendChild(bookInfoNode);
+					bookNode.appendChild(bookContainerNode);
+					//var nodeContent = "<div class=\"col-8\" ><div class=\"book-info\">"+book.volumeInfo.title+"</div><div>"
+					//+"<a href=\""+ book.volumeInfo.canonicalVolumeLink +"\" target=\"_blank\" class=\"btn btn-info book-link\" role=\"button\">See this Book</a>"+"</div></div></div>";
+					//nodeContent = "<div class=\"row book\" ><div class=\"col-4\" >"+imgHTML+"</div>"+nodeContent;
 					//var textnode = document.createTextNode(book.volumeInfo.title);       
 					//node.appendChild(textnode); 
-					node.innerHTML = nodeContent;                           
-					book_container.appendChild(node);
+					//node.innerHTML = nodeContent;                           
+					book_container.appendChild(bookNode);
 				});
 			});
-			/*.done(function() {
-				alert( "second success" );
-			  })
-			  .fail(function() {
-				alert( "error" );
-			  })
-			  .always(function() {
-				alert( "finished" );
-			  });*/
 		}else
 		{
 			$("#error").show();
@@ -55,3 +92,8 @@ $( document ).ready(function() {
 	});
 
 });
+
+function toggleTest()
+{
+	$("#results-test").toggle();
+}
